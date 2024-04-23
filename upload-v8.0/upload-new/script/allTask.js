372b4db906cb403765ea62e94793246433b7fe61
Haoran Zu <529261931@qq.com>
2024-04-23 13:10:26 -0400

    change search function

--- upload-v8.0/upload-new/script/allTask.js
+++ upload-v8.0/upload-new/script/allTask.js
@@ -11,9 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
         // Loop through rows and hide/show based on search value
         rows.forEach(function(row) {
             var taskName = row.cells[0].innerText.trim().toLowerCase(); // Assuming task name is in the first column
-
+            var taskInfo = row.cells[1].innerText.trim().toLowerCase(); // Assuming task info is in the second column
+            var taskPriority = row.cells[2].innerText.trim().toLowerCase(); // Assuming task priority level is in the third column
+            var userName = row.cells[4].innerText.trim().toLowerCase(); // Assuming user name is in the fourth column
             // Check if task name contains the search value
-            if (taskName.includes(searchValue)) {
+            if (taskName.includes(searchValue) | taskInfo.includes(searchValue) | taskPriority.includes(searchValue) | userName.includes(searchValue)) {
                 row.style.display = ""; // Show the row
             } else {
                 row.style.display = "none"; // Hide the row
